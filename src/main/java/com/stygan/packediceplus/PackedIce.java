5285b817046598388e61ec9cfd1ae5666ed5fcd1
Stygan <javier.stuca@gmail.com>
2020-03-17 04:45:17 -0300

    Initial commit

---
+++ src/main/java/com/stygan/packediceplus/PackedIce.java
@@ -0,0 +1,56 @@
+package com.stygan.packediceplus;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("packediceplus")	
+@Mod.EventBusSubscriber(modid = PackedIce.MOD_ID, bus = Bus.MOD)
+public class PackedIce
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "packediceplus"; 
+    public static PackedIce instance;
+    
+    public PackedIce() 
+    {
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    	modEventBus.addListener(this::setup);
+    	modEventBus.addListener(this::doClientStuff);
+        
+        instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+    	
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) 
+    {
+
+    }
+    
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) 
+    {
+    	
+    }
+    
+    @SubscribeEvent
+    public static void loadCompleteEvent(FMLLoadCompleteEvent event	) 
+    {
+
+    }
+}
---
+++ src/main/java/com/stygan/packediceplus/init/BlockInit.java
@@ -0,0 +1,46 @@
+package com.stygan.packediceplus.init;
+
+import com.stygan.packediceplus.PackedIce;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SlabBlock;
+import net.minecraft.block.StairsBlock;
+import net.minecraft.block.WallBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(PackedIce.MOD_ID)
+@Mod.EventBusSubscriber(modid = PackedIce.MOD_ID, bus = Bus.MOD)
+public class BlockInit {
+	public static final Block ice_stairs = null;
+	public static final Block ice_slab = null;
+	public static final Block ice_wall = null;
+
+	@SubscribeEvent
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+		event.getRegistry()
+				.register(new StairsBlock(Blocks.PACKED_ICE::getDefaultState, Block.Properties.from(Blocks.PACKED_ICE))
+						.setRegistryName("ice_stairs"));
+		event.getRegistry()
+				.register(new SlabBlock(Block.Properties.from(Blocks.PACKED_ICE)).setRegistryName("ice_slab"));
+		event.getRegistry()
+				.register(new WallBlock(Block.Properties.from(Blocks.PACKED_ICE)).setRegistryName("ice_wall"));
+	}
+
+	@SubscribeEvent
+	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(new BlockItem(ice_stairs, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+				.setRegistryName("ice_stairs"));
+		event.getRegistry().register(new BlockItem(ice_slab, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+				.setRegistryName("ice_slab"));
+		event.getRegistry().register(new BlockItem(ice_wall, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+				.setRegistryName("ice_wall"));
+	}
+}
